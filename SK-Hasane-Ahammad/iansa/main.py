c72625b72e39a67d1c020429d7ff81fde6e9bb60
phfr24 <phfr24@inf.ufpr.br>
2025-02-12 18:12:17 -0300

    add preprocess()

--- SK-Hasane-Ahammad/iansa/main.py
+++ SK-Hasane-Ahammad/iansa/main.py
@@ -1,4 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
+def preprocess(dataframe: pd.DataFrame) -> pd.DataFrame:
+    dataframe = dataframe.replace({False: 0, True: 1})
+    dataframe = dataframe.drop_duplicates()
+
+    # URL column is not needed for model training
+    dataframe = dataframe.drop(columns=['url'])
+
+    # normalize every column except 'label', 'has_ip', 'has_port'
+    cols_to_normalize = dataframe.columns.difference(['label', 'has_ip', 'has_port'])
+    scaler = MinMaxScaler()
+    dataframe[cols_to_normalize] = scaler.fit_transform(dataframe[cols_to_normalize])
+
+    return dataframe
 
 def main():
     pass
