6dbe5b9f2fc4a10aed4d5984d5916dd9b7631c65
phfr24 <phfr24@inf.ufpr.br>
2025-02-12 16:33:09 -0300

    refactor features.py

--- SK-Hasane-Ahammad/iansa/src/features.py
+++ SK-Hasane-Ahammad/iansa/src/features.py
@@ -1,60 +1,56 @@
-
 from urllib.parse import urlparse
-import pandas
+import pandas as pd
 import re
 
-class Features:
+class FeatureExtractor:
 
     path: str
-    csvf: pandas.DataFrame
+    datf: pd.DataFrame
 
     def __init__(self, path: str):
-        self.csvf = pandas.read_csv(path)
         self.path = path
-
+        self.datf = pd.read_csv(path)
 
     def extract(self):
         
-        def hasIP(url: str) -> bool:
+        def has_ip(url: str) -> bool:
             pattern = r'((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}' # ipv4 pattern
             match = re.search(pattern, url)
 
             return (match is not None)
 
-        def numberCount():
+        def number_count(url: str) -> int:
             hostname = urlparse(url).hostname
+
             return sum(c.isdigit() for c in hostname)
 
-        def dashSymbolCount():
+        def dash_symbol_count(url: str) -> int:
             hostname = urlparse(url).hostname
+
             return hostname.count('-')
 
-        def urlLength(url: str) -> int:
+        def url_length(url: str) -> int:
             return len(url)
 
-        def urlDepth(url: str) -> int:
+        def url_depth(url: str) -> int:
             path = urlparse(url).path
             segments = [s for s in path.split('/') if s] # discard empty strings
+
             return len(segments)
 
-        def subdomainCount(url: str) -> int:
+        def subdomain_count(url: str) -> int:
             hostname = urlparse(url).hostname
             labels = [l for l in hostname.split('.') if l]
 
             return (len(labels) - 2) # exclude TLD and SLD
 
-        def queryParamsCount(url: str) -> int:
+        def query_params_count(url: str) -> int:
             query = urlparse(url).query
             params = [p for p in query.split('&') if p]
 
             return len(params)
 
-        # bernas
-        def randomness():
-            hostname = urlparse(url).hostname
-            pass
-
-        def hasPort():
+        def has_port(url: str) -> bool:
             port = urlparse(url).port
-            return (port is not None)
 
+            return (port is not None)
