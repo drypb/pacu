0abd23fd243bb8dc90f3892e74bb4e45de015d18
davi <dcr23@inf.ufpr.br>
2025-03-20 13:22:24 -0300

    add kolmogorov-smirnov and distance features

--- iansa/features.py
+++ iansa/features.py
@@ -1,8 +1,17 @@
 from urllib.parse import urlparse
 import pandas as pd
 import re
+import string
+import scipy
 
 ipv4_pattern = re.compile(r'((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}')
+
+frequency_ptbr = [ 0.1463, 0.0104, 0.0388, 0.0499,
+    0.1257, 0.0102, 0.0130, 0.0128, 0.0618, 0.0040,
+    0.0002, 0.0278, 0.0474, 0.0505, 0.1073, 0.0252,
+    0.0120, 0.0653, 0.0781, 0.0434, 0.0463, 0.0167,
+     0.0001, 0.0021, 0.0001, 0.0047]
+
 
 def has_ip(url: str) -> bool:
     match = re.search(ipv4_pattern, url)
@@ -38,6 +47,48 @@ def has_port(url: str) -> bool:
     port = urlparse(url).port
     return (port is not None)
 
+def char_distribution(url: str) -> list:
+    url=re.sub(urlparse(url).scheme, "", url) # remove scheme  
+    url=re.sub("\\W|\\d", "", url) # remove non-alphabetical char
+
+    dist = []
+    for char in string.ascii_lowercase:
+        dist.append(url.lower().count(char)/26)
+    
+    return dist;
+
+def kolmogorov_smirnov(url: str, dist: list) -> float:
+    url_dist = char_distribution(url)
+    result = scipy.stats.ks_2samp(url_dist, dist)
+
+    return result[0]
+
+def kullback_leibler(url: str, dist: list) -> float:
+    url_dist = char_distribution(url)
+    result = scipy.stats.entropy(url_dist, dist)
+
+    return result[0]
+
+def euclidean_dist(url: str, dist: list) -> float:
+    url_dist = char_distribution(url)
+    result = scipy.distance.euclidean(url_dist, dist)
+
+    return result[0]
+
+
+def cheby_shev_distance(url: str, dist: list) -> float:
+    url_dist = char_distribution(url)
+    result = scipy.distance.chebyshev(url_dist, dist)
+
+    return result[0]
+
+def manhattan_dist(url: str, dist: list) -> float:
+    url_dist = char_distribution(url)
+    result = scipy.distance.cityblock(url_dist, dist)
+
+    return result[0]
+
+
 def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     df['has_ip'] = df['url'].apply(has_ip)
     df['number_count'] = df['url'].apply(number_count)
@@ -48,3 +99,7 @@ def extract_features(df: pd.DataFrame) -> pd.DataFrame:
     df['query_params_count'] = df['url'].apply(query_params_count)
     df['has_port'] = df['url'].apply(has_port)
     return df
+
+
+
+
