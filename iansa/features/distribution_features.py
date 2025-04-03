d254afeb1c41fd2d4309fdf7ba22beaf948e2cbe
davi <dcr23@inf.ufpr.br>
2025-04-03 16:29:36 -0300

    refactor distribution extraction to consider printable characteres

--- iansa/features/distribution_features.py
+++ iansa/features/distribution_features.py
@@ -11,8 +11,7 @@ import scipy
 # Replace all special symbols and numbers with a chosen replacement
 def strip_url(url: str, replacement_char: str = "") -> str:
     url=re.sub(urlparse(url).scheme, "", url) # remove scheme  
-    url=re.sub("\\W|\\d", replacement_char, url) # remove non-alphabetical char
-
+    url=re.sub("://", "", url)
     return url
 
 # Calculates the distrubution of letters in the url 
@@ -20,39 +19,26 @@ def char_dist(url: str) -> list:
     url=strip_url(url)
 
     dist = []
-    for char in string.ascii_lowercase:
-        dist.append(url.lower().count()/url.length()) 
+    for char in string.printable[:-6]:
+        dist.append(url.count(char)/len(url)) 
     
     return dist;
 
-
-# Calculates the number of bigrams in the url
-def bigram_count(url: str, delimeter: str = " ") -> int:
-    bag_of_words = url.split(delimeter)
-
-    return sum(list(map(lambda x: x.length() - 1, bag_of_words)))
-
-# Calculates the index of a given bigram in a vector 26*26 elements long
-def bigram_index(bigram: str) -> int:
-    return ord(bigram[0].lower())%0x61*26 + ord(bigram[1].lower())
-
 # Calculate the bigram distribution for a given url
 def bigram_dist(url: str) -> list:
-    delim = "."
+    url = strip_url(url)    
+    bigrams = [0]*len(string.printable)**2 
 
-    url = strip_url(url, delim)    
-    bigrams = [0] * 26 * 26
-
-    distribution_unit = 1/bigram_count(url, delim)
+    distribution_unit = 1/(len(url)-1)
  
-    for i in enumerate(url[:-1]):
-        if delim not in url[i:i+1]: 
-            bigrams[bigram_index(url[i:i+1])] += distribution_unit
+    for i, char in enumerate(url[:-1]):
+        idx1 = string.printable.find(url[i])*len(string.printable[:-6])
+        idx2 = string.printable.find(url[i+1])
+        bigrams[idx1 + idx2] += distribution_unit
 
     return bigrams
 
 # Actual features 
-
 def kolmogorov_smirnov(url: str, calc_dist: Callable[[str],list], dist: list) -> float:
     url_dist = calc_dist(url)
     result = scipy.stats.ks_2samp(url_dist, dist)
