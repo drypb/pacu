2796e136cc27d0d0c557223c1a75f767e59f1746
pedro f <phfr24@inf.ufpr.br>
2025-04-23 15:26:03 -0300

    fix formatting and use more descriptive var names for normal distributions

--- src/pacu/features/features.py
+++ src/pacu/features/features.py
@@ -4,6 +4,10 @@ from pacu.features.distributions import *
 
 import pandas as pd
 
+# TODO make this configurable
+normal_bigram_dist = bigram_distribution_br
+normal_char_dist = char_distribution_br
+
 def process_row(row: pd.Series) -> pd.Series:
     url = row["url"]
     url_s = strip_url(url)
@@ -19,16 +23,16 @@ def process_row(row: pd.Series) -> pd.Series:
         "subdomain_count"   : subdomain_count(url),
         "query_params_count": query_params_count(url),
         "has_port"          : has_port(url),
-        "ks_char"           : kolmogorov_smirnov(url_s, chardist, frequency_char_unlabeled),
-        "kl_char"           : kullback_leibler(url_s, chardist, frequency_char_unlabeled),
-        "eucli_char"        : euclidean_dist(url_s, chardist, frequency_char_unlabeled),
-        "cs_char"           : cheby_shev_dist(url_s, chardist, frequency_char_unlabeled),
-        "man_char"          : manhattan_dist(url_s, chardist, frequency_char_unlabeled),
-        "ks_big"            : kolmogorov_smirnov(url_s, bigdist, frequency_bigram_unlabeled),
-        "kl_big"            : kullback_leibler(url_s, bigdist, frequency_bigram_unlabeled),
-        "eucli_big"         : euclidean_dist(url_s, bigdist, frequency_bigram_unlabeled),
-        "cs_big"            : cheby_shev_dist(url_s, bigdist, frequency_bigram_unlabeled),
-        "man_big"           : manhattan_dist(url_s, bigdist, frequency_bigram_unlabeled),
+        "ks_char"           : kolmogorov_smirnov(url_s, chardist, normal_char_dist),
+        "kl_char"           : kullback_leibler(url_s, chardist, normal_char_dist),
+        "eucli_char"        : euclidean_dist(url_s, chardist, normal_char_dist),
+        "cs_char"           : cheby_shev_dist(url_s, chardist, normal_char_dist),
+        "man_char"          : manhattan_dist(url_s, chardist, normal_char_dist),
+        "ks_big"            : kolmogorov_smirnov(url_s, bigdist, normal_bigram_dist),
+        "kl_big"            : kullback_leibler(url_s, bigdist, normal_bigram_dist),
+        "eucli_big"         : euclidean_dist(url_s, bigdist, normal_bigram_dist),
+        "cs_big"            : cheby_shev_dist(url_s, bigdist, normal_bigram_dist),
+        "man_big"           : manhattan_dist(url_s, bigdist, normal_bigram_dist),
         "huffman"           : huffman(fq),
         "label"             : row["label"]
     })
